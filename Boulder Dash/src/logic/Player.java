fc0476aa588f471598c64c132a580bf5a5b6c11e
Dygwah98 <36772959+Dygwah98@users.noreply.github.com>
2019-05-17 01:11:49 +0200

    More natural Player movement

--- Boulder Dash/src/logic/Player.java
+++ Boulder Dash/src/logic/Player.java
@@ -6,6 +6,7 @@ import graphics.Sprite;
 
 public class Player extends Living {
 
+	private int pastDir = -1;
 	public LivingSprite ls;
 	
 	public Player(int x, int y, int speed) {
@@ -19,6 +20,7 @@ public class Player extends Living {
 	@Override
 	public boolean move(int dir) {
 		
+		if(pastDir == -1) pastDir = dir;
 		//int i = (x + dmap[dir][0] * speed + map.dimX*Sprite.TILE_SIZE)%(map.dimX*Sprite.TILE_SIZE);
 		//int j = (y + dmap[dir][1] * speed + map.dimY*Sprite.TILE_SIZE)%(map.dimY*Sprite.TILE_SIZE);
 		
@@ -30,13 +32,17 @@ public class Player extends Living {
 		
 		if(map.getTile(i, j).getType() == Block.EMPTY_BLOCK) {
 			
-		//	if(movecount < speed)	
-				//++movecount;
-			//else {
-			//	movecount = 0;
+			if(movecount < speed && pastDir == dir)	
+				++movecount;
+			else if(pastDir != dir) {
+				pastDir = dir;
+				movecount = 0;
+			}
+			else {
+				movecount = 0;
 				x = i;
 				y = j;
-			//}
+			}
 			
 			return true;
 		}
