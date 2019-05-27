ffb97f87717636821b6473b14b1b67a5ec6cd535
Dygwah98 <36772959+Dygwah98@users.noreply.github.com>
2019-05-28 00:54:26 +0200

    Cleared map loading methods
    -Fixed a minor issue with Map.initialize()

--- Boulder Dash/src/logic/Map.java
+++ Boulder Dash/src/logic/Map.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
 
 //definisce la mappa di gioco come matrice di blocchi
 public class Map {
@@ -19,18 +18,19 @@ public class Map {
 	// numero diamanti sulla mappa (per la condizione di vittoria)
 	int numDiamonds;
 
-	// carica mappa da file di testo e la salva in una mappa di string
-	private char[][] load(String filename) {
+	private void initialize(String filename) {
 
-		// tentativo di inizializzare il fileBuffer
+		numDiamonds = 0;
+
 		BufferedReader bIn = null;
 		try {
 			bIn = new BufferedReader(new FileReader(defaultPath + filename));
 		} catch (FileNotFoundException e) { // trovare una gestione migliore
-			e.printStackTrace();
+			map = null;
+			dimX = -1;
+			dimY = -1;
+			return;
 		}
-
-		char[][] level = null;
 
 		// se e' inizializzato
 		if (bIn != null) {
@@ -41,78 +41,49 @@ public class Map {
 				if (bIn.ready()) {
 					dimX = Integer.parseInt(bIn.readLine());
 					dimY = Integer.parseInt(bIn.readLine());
+					
+					map = new Block[dimX][dimY];
 
 					System.out.println(dimX);
 					System.out.println(dimY);
 
-					// si inizializza la matrice
-					level = new char[dimX][dimY];
-
-					while (bIn.ready())
-						for (int i = 0; i < dimX; i++) {
-							// si legge la casella
+					while (bIn.ready()) {
+						for (int x = 0; x < dimX; x++) {
 							line = bIn.readLine();
-							for (int j = 0; j < dimY; j++)
-								level[i][j] = line.charAt(j);
-
+							for (int y = 0; y < dimY; y++) {
+								switch (line.charAt(y)) {
+									case Block.DIAMOND:
+										map[x][y] = new Diamond(x, y);
+										++numDiamonds;
+										break;
+									case Block.GROUND:
+										map[x][y] = new Ground(x, y);
+										break;
+									case Block.WALL:
+										map[x][y] = new Wall(x, y);
+										break;
+									case Block.ROCK:
+										map[x][y] = new Rock(x, y);
+										break;
+									case Block.EMPTY_BLOCK:
+										map[x][y] = Destructible.emptyTile;
+										break;
+								}
+							}
 						}
+					}
 					bIn.close();
-
 				}
 			} catch (Exception e) {
 				// TODO: handle exception
 				e.printStackTrace();
 			}
-		}
-
-		return level;
-
-	}
-
-	private void initialize(String filename) {
-
-		numDiamonds = 0;
-
-		char level[][] = load(filename);
-		
-		/*for(int i = 0; i < dimX; i++) {
-			for(int j = 0; j < dimY; j++)
-				System.out.print(level[i][j]);
-			System.out.print("\n");
-		}*/
-
-		map = new Block[dimX][dimY];
-
-		for (int x = 0; x < dimX; x++)
-			for (int y = 0; y < dimY; y++) {
-				//System.out.println(x + " - " + y);
-				switch (level[x][y]) {
-				case Block.DIAMOND:
-					map[x][y] = new Diamond(x, y);
-					++numDiamonds;
-					break;
-				case Block.GROUND:
-					map[x][y] = new Ground(x, y);
-					break;
-				case Block.WALL:
-					map[x][y] = new Wall(x, y);
-					break;
-				case Block.ROCK:
-					map[x][y] = new Rock(x, y);
-					break;
-				case Block.EMPTY_BLOCK:
-					map[x][y] = Destructible.emptyTile;
-					break;
-				}
-			}
-				
+		}			
 	}
 
 	public Map(String filename) {
-		if (Block.map == null)
-			Block.map = this;
-		if (Living.map == null)
-			Living.map = this;
+		if(Block.map == null)  Block.map = this;
+		if(Living.map == null) Living.map = this;
 
 		initialize(filename);
 	}
