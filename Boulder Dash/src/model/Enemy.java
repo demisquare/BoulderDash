e55f39f579ce6da9c2eddae214b524922a586d13
Dygwah98 <36772959+Dygwah98@users.noreply.github.com>
2019-06-23 17:55:44 +0200

    Merge

--- Boulder Dash/src/model/Enemy.java
+++ Boulder Dash/src/model/Enemy.java
@@ -6,6 +6,7 @@ import java.util.Random;
 public class Enemy extends GameObject implements Living {
 	
 	//manca anche qui un delay sul movimento: con un counter oppure usando ScheduledExecutorService?
+	private int delayMovement;
 	
 	protected Random r;
 	protected int speed;
@@ -17,26 +18,32 @@ public class Enemy extends GameObject implements Living {
 		speed = s;
 		r = new Random();
 		lastDir = 0;
+		delayMovement = 0;
 	}
 
 	@Override
 	protected boolean move(int dir) {
 		
-		int i = dmap[dir][0];
-		int j = dmap[dir][1];
+		int i = x + dmap[dir][0];
+		int j = y + dmap[dir][1];
 //		int x1 = (x+j >= 0 && x+j < map.dimX) ? x+j : -1;
 //		int x2 = (x-j >= 0 && x-j < map.dimX) ? x-j : -1;
 //		int y1 = (y+i >= 0 && y+i < map.dimY) ? y+i : -1;
 //		int y2 = (y-i >= 0 && y-i < map.dimY) ? y-i : -1;
-		
-		/*if((x1 != -1 && y1 != -1 && !(map.getTile(x1, y1) instanceof EmptyBlock)) ||
+/*		
+		if((x1 != -1 && y1 != -1 && !(map.getTile(x1, y1) instanceof EmptyBlock)) ||
 		   (x1 != -1 && y2 != -1 && !(map.getTile(x1, y2) instanceof EmptyBlock)) ||
 		   (x2 != -1 && y1 != -1 && !(map.getTile(x2, y1) instanceof EmptyBlock)) ||
 		   (x2 != -1 && y2 != -1 && !(map.getTile(x2, y2) instanceof EmptyBlock))) {
-		*/
+*/		
 			if(!(i < 0 || i >= map.dimX) && !(j < 0 || j >= map.dimY)) {
 				
-				//???
+				if(map.getTile(i, j) instanceof Player) {
+					
+					map.getTile(i, j).destroy();
+					swap(i, j);
+					return true;
+				}
 			}
 			
 			return super.move(dir);
@@ -44,11 +51,19 @@ public class Enemy extends GameObject implements Living {
 		
 		//return false;
 	}
-	
+
 	@Override
 	public boolean update() {
-		lastDir = r.nextInt(4);
-		return move(lastDir);
+		
+		++delayMovement;
+		if(delayMovement == 2) {
+			
+			delayMovement = 0;
+			lastDir = r.nextInt(4);
+			return move(lastDir);
+		}
+		
+		return false;
 	}
 
 	@Override
