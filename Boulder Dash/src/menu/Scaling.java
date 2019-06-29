8ac3337b711fb698c5e9c26bda1b7da3222cff20
Dygwah98 <36772959+Dygwah98@users.noreply.github.com>
2019-06-29 14:55:37 +0200

    minor fixes

---
+++ Boulder Dash/src/menu/Scaling.java
@@ -0,0 +1,43 @@
+package menu;
+
+import java.awt.Image;
+import java.awt.Toolkit;
+
+import javax.swing.JLabel;
+
+public class Scaling {
+
+	private static double vx;
+	private static double vy;
+	
+	private Scaling() {}
+	
+	private static void updateToolkit() {
+		
+		double tx = Toolkit.getDefaultToolkit().getScreenSize().getWidth()/1280d;
+		if(tx != vx) vx = tx;
+	
+		double ty = Toolkit.getDefaultToolkit().getScreenSize().getHeight()/720d;
+		if(ty != vy) vy = ty;
+	}
+	
+	public static Image get(Image img, int a, int b, boolean scaled) {
+		
+		if(scaled) {
+			updateToolkit();
+			return img.getScaledInstance((int)(a*vx), (int)(b*vy), Image.SCALE_SMOOTH);		
+		}
+		
+		return img.getScaledInstance(a, b, Image.SCALE_SMOOTH);
+	}
+	
+	public static void set(JLabel L, int a, int b, int c, int d, boolean scaled) {
+		
+		if(scaled) {
+			updateToolkit();
+			L.setBounds((int)(a*vx), (int)(b*vy), c, d);	
+		
+		} else
+			L.setBounds(a, b, c, d);
+	}
+}
