3a66c6d32200f0482f6cefa66f40098e8c4b6fb8
DavidG33k <davide.gena@gmail.com>
2019-07-01 01:28:09 +0200

    Code improvements
    ...

--- Boulder Dash/src/menu/Menu.java
+++ Boulder Dash/src/menu/Menu.java
@@ -129,15 +129,9 @@ public class Menu extends JPanel implements Serializable {
 		}
 	}
 
-	public Menu(JFrame frame) {
-		options  = new Options(frame, this);
-		credits  = new Credits(frame, this);
-		game 	 = new Game(frame, this);
-		multi	 = new Multiplayer(frame, game, this);
-		
+	private void image_init () {
 		try {
 			background = ImageIO.read(new File(MainMenuPage + "MainMenuPage_Background.png"));
-
 			START = ImageIO.read(new File(MainMenuPage + "MainMenuPage_START.png")).getScaledInstance(191, 55, Image.SCALE_SMOOTH);
 			MULTI = ImageIO.read(new File(MainMenuPage + "MainMenuPage_MULTI.png")).getScaledInstance(423, 55, Image.SCALE_SMOOTH);
 			OPTIONS = ImageIO.read(new File(MainMenuPage + "MainMenuPage_OPTIONS.png")).getScaledInstance(272, 54, Image.SCALE_SMOOTH);
@@ -148,237 +142,257 @@ public class Menu extends JPanel implements Serializable {
 			OPTIONS_SELECTED = ImageIO.read(new File(MainMenuPage + "MainMenuPage_OPTIONS_SELECTED.png")).getScaledInstance(272, 54, Image.SCALE_SMOOTH);
 			CREDITS_SELECTED = ImageIO.read(new File(MainMenuPage + "MainMenuPage_CREDITS_SELECTED.png")).getScaledInstance(267, 55, Image.SCALE_SMOOTH);
 			EXIT_SELECTED = ImageIO.read(new File(MainMenuPage + "MainMenuPage_EXIT_SELECTED.png")).getScaledInstance(141, 50, Image.SCALE_SMOOTH);
-
-			START_scaled = new JLabel(new ImageIcon(START));
-			MULTI_scaled = new JLabel(new ImageIcon(MULTI));
-			OPTIONS_scaled = new JLabel(new ImageIcon(OPTIONS));
-			CREDITS_scaled = new JLabel(new ImageIcon(CREDITS));
-			EXIT_scaled = new JLabel(new ImageIcon(EXIT));
-
-			START_scaled.addMouseListener(new MouseListener() {
-
-				@Override
-				public void mouseEntered(MouseEvent e) {
-					synchronized(this) {
-						Music.playTone("hover");
-					}
-					START_scaled.setIcon(new ImageIcon(Scaling.get(START_SELECTED, 191, 55, Options.full_screen)));
-					revalidate();
-					repaint();
-				}
-
-				@Override
-				public void mouseExited(MouseEvent e) {
-					START_scaled.setIcon(new ImageIcon(Scaling.get(START, 191, 55, Options.full_screen)));
-					revalidate();
-					repaint();
-				}
-
-				@Override
-				public void mousePressed(MouseEvent e) {
-					
-					synchronized(this) {
-						Music.playTone("select");
-					}
-					
-					try {
-						start_selected(frame);
-					} catch (Exception e1) {
-						e1.printStackTrace();
-					}
-					
-					START_scaled.setIcon(new ImageIcon(Scaling.get(START, 191, 55, Options.full_screen)));
-					revalidate();
-					repaint();
-				}
-
-				@Override public void mouseReleased(MouseEvent e) 	{}
-				@Override public void mouseClicked(MouseEvent e) 	{}
-			});
-			
-			MULTI_scaled.addMouseListener(new MouseListener() {
-
-				@Override
-				public void mousePressed(MouseEvent e) {
-					
-					synchronized(this) {
-						Music.playTone("select");
-					}
-					
-					try {
-						multi_selected(frame, multi, youlose);
-					} catch (InterruptedException e1) {
-						e1.printStackTrace();
-					}
-					
-					MULTI_scaled.setIcon(new ImageIcon(Scaling.get(MULTI, 423, 55, Options.full_screen)));
-					revalidate();
-					repaint();
-				}
-
-				@Override
-				public void mouseExited(MouseEvent e) {
-					MULTI_scaled.setIcon(new ImageIcon(Scaling.get(MULTI, 423, 55, Options.full_screen)));
-					revalidate();
-					repaint();
-				}
-
-				@Override
-				public void mouseEntered(MouseEvent e) {
-					
-					synchronized(this) {
-						Music.playTone("hover");
-					}
-					
-					MULTI_scaled.setIcon(new ImageIcon(Scaling.get(MULTI_SELECTED, 423, 55, Options.full_screen)));
-					revalidate();
-					repaint();
-				}
-
-				@Override public void mouseClicked(MouseEvent e) {}
-				@Override public void mouseReleased(MouseEvent e) {}
-			});
-			
-			OPTIONS_scaled.addMouseListener(new MouseListener() {
-
-				@Override
-				public void mousePressed(MouseEvent e) {
-					
-					synchronized(this) {
-						Music.playTone("select");
-					}
-					
-					try {
-						options_selected(frame, options);
-					} catch (InterruptedException e1) {
-						e1.printStackTrace();
-					}
-					
-					OPTIONS_scaled.setIcon(new ImageIcon(Scaling.get(OPTIONS, 272, 54, Options.full_screen)));
-					revalidate();
-					repaint();
-				}
-
-				@Override
-				public void mouseExited(MouseEvent e) {
-					OPTIONS_scaled.setIcon(new ImageIcon(Scaling.get(OPTIONS, 272, 54, Options.full_screen)));
-					revalidate();
-					repaint();
-				}
-
-				@Override
-				public void mouseEntered(MouseEvent e) {
-					
-					synchronized(this) {
-						Music.playTone("hover");
-					}
-					
-					OPTIONS_scaled.setIcon(new ImageIcon(Scaling.get(OPTIONS_SELECTED, 272, 54, Options.full_screen)));
-					revalidate();
-					repaint();
-				}
-
-				@Override public void mouseClicked(MouseEvent e) 	{}
-				@Override public void mouseReleased(MouseEvent e) 	{}
-			});
-			
-			CREDITS_scaled.addMouseListener(new MouseListener() {
-
-				@Override
-				public void mousePressed(MouseEvent e) {
-					
-					synchronized(this) {
-						Music.playTone("select");
-					}
-					
-					try {
-						credits_selected(frame, credits);
-					} catch (InterruptedException e1) {
-						e1.printStackTrace();
-					}
-					
-					CREDITS_scaled.setIcon(new ImageIcon(Scaling.get(CREDITS, 267, 55, Options.full_screen)));
-					revalidate();
-					repaint();
-				}
-
-				@Override
-				public void mouseExited(MouseEvent e) {
-					CREDITS_scaled.setIcon(new ImageIcon(Scaling.get(CREDITS, 267, 55, Options.full_screen)));
-					revalidate();
-					repaint();
-				}
-
-				@Override
-				public void mouseEntered(MouseEvent e) {
-					
-					synchronized(this) {
-						Music.playTone("hover");
-					}
-					
-					CREDITS_scaled.setIcon(new ImageIcon(Scaling.get(CREDITS_SELECTED, 267, 55, Options.full_screen)));
-					revalidate();
-					repaint();
-				}
-
-				@Override public void mouseClicked(MouseEvent e) 	{}
-				@Override public void mouseReleased(MouseEvent e) 	{}
-			});
-			
-			EXIT_scaled.addMouseListener(new MouseListener() {
-
-				@Override
-				public void mouseEntered(MouseEvent e) {
-					
-					synchronized(this) {
-						Music.playTone("hover");
-					}
-					
-					EXIT_scaled.setIcon(new ImageIcon(Scaling.get(EXIT_SELECTED, 141, 50, Options.full_screen)));
-					revalidate();
-					repaint();
-				}
-
-				@Override
-				public void mouseExited(MouseEvent e) {
-					EXIT_scaled.setIcon(new ImageIcon(Scaling.get(EXIT, 141, 50, Options.full_screen)));
-					revalidate();
-					repaint();
-				}
-
-				@Override
-				public void mousePressed(MouseEvent e) {
-					
-					synchronized(this) {
-						Music.playTone("select");
-					}
-					
-					System.exit(0);
-				}
-
-				@Override public void mouseReleased(MouseEvent e) 	{}
-				@Override public void mouseClicked(MouseEvent e) 	{}
-			});
-
-			menu_choices = new JPanel();
-			menu_choices.setLayout(new BoxLayout(menu_choices, BoxLayout.Y_AXIS));
-
-			menu_choices.add(START_scaled);
-			menu_choices.add(MULTI_scaled);
-			menu_choices.add(OPTIONS_scaled);
-			menu_choices.add(CREDITS_scaled);
-			menu_choices.add(EXIT_scaled);
-
-			this.setLayout(null);
-			this.add(menu_choices);
-			menu_choices.setBackground(new Color(0, 0, 0, 0));
-			menu_choices.setBounds((1280 / 2 - 430 / 2), 250, 550, 550);
-			
-//			launchThread();
-			
 		} catch (IOException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+	
+	private void label_init() {
+		START_scaled = new JLabel(new ImageIcon(START));
+		MULTI_scaled = new JLabel(new ImageIcon(MULTI));
+		OPTIONS_scaled = new JLabel(new ImageIcon(OPTIONS));
+		CREDITS_scaled = new JLabel(new ImageIcon(CREDITS));
+		EXIT_scaled = new JLabel(new ImageIcon(EXIT));
+	}
+	
+	private void MouseListener_init (JFrame frame) {
+		START_scaled.addMouseListener(new MouseListener() {
+
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				synchronized(this) {
+					Music.playTone("hover");
+				}
+				START_scaled.setIcon(new ImageIcon(Scaling.get(START_SELECTED, 191, 55, Options.full_screen)));
+				revalidate();
+				repaint();
+			}
+
+			@Override
+			public void mouseExited(MouseEvent e) {
+				START_scaled.setIcon(new ImageIcon(Scaling.get(START, 191, 55, Options.full_screen)));
+				revalidate();
+				repaint();
+			}
+
+			@Override
+			public void mousePressed(MouseEvent e) {
+				
+				synchronized(this) {
+					Music.playTone("select");
+				}
+				
+				try {
+					start_selected(frame);
+				} catch (Exception e1) {
+					e1.printStackTrace();
+				}
+				
+				START_scaled.setIcon(new ImageIcon(Scaling.get(START, 191, 55, Options.full_screen)));
+				revalidate();
+				repaint();
+			}
+
+			@Override public void mouseReleased(MouseEvent e) 	{}
+			@Override public void mouseClicked(MouseEvent e) 	{}
+		});
+		
+		MULTI_scaled.addMouseListener(new MouseListener() {
+
+			@Override
+			public void mousePressed(MouseEvent e) {
+				
+				synchronized(this) {
+					Music.playTone("select");
+				}
+				
+				try {
+					multi_selected(frame, multi, youlose);
+				} catch (InterruptedException e1) {
+					e1.printStackTrace();
+				}
+				
+				MULTI_scaled.setIcon(new ImageIcon(Scaling.get(MULTI, 423, 55, Options.full_screen)));
+				revalidate();
+				repaint();
+			}
+
+			@Override
+			public void mouseExited(MouseEvent e) {
+				MULTI_scaled.setIcon(new ImageIcon(Scaling.get(MULTI, 423, 55, Options.full_screen)));
+				revalidate();
+				repaint();
+			}
+
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				
+				synchronized(this) {
+					Music.playTone("hover");
+				}
+				
+				MULTI_scaled.setIcon(new ImageIcon(Scaling.get(MULTI_SELECTED, 423, 55, Options.full_screen)));
+				revalidate();
+				repaint();
+			}
+
+			@Override public void mouseClicked(MouseEvent e) {}
+			@Override public void mouseReleased(MouseEvent e) {}
+		});
+		
+		OPTIONS_scaled.addMouseListener(new MouseListener() {
+
+			@Override
+			public void mousePressed(MouseEvent e) {
+				
+				synchronized(this) {
+					Music.playTone("select");
+				}
+				
+				try {
+					options_selected(frame, options);
+				} catch (InterruptedException e1) {
+					e1.printStackTrace();
+				}
+				
+				OPTIONS_scaled.setIcon(new ImageIcon(Scaling.get(OPTIONS, 272, 54, Options.full_screen)));
+				revalidate();
+				repaint();
+			}
+
+			@Override
+			public void mouseExited(MouseEvent e) {
+				OPTIONS_scaled.setIcon(new ImageIcon(Scaling.get(OPTIONS, 272, 54, Options.full_screen)));
+				revalidate();
+				repaint();
+			}
+
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				
+				synchronized(this) {
+					Music.playTone("hover");
+				}
+				
+				OPTIONS_scaled.setIcon(new ImageIcon(Scaling.get(OPTIONS_SELECTED, 272, 54, Options.full_screen)));
+				revalidate();
+				repaint();
+			}
+
+			@Override public void mouseClicked(MouseEvent e) 	{}
+			@Override public void mouseReleased(MouseEvent e) 	{}
+		});
+		
+		CREDITS_scaled.addMouseListener(new MouseListener() {
+
+			@Override
+			public void mousePressed(MouseEvent e) {
+				
+				synchronized(this) {
+					Music.playTone("select");
+				}
+				
+				try {
+					credits_selected(frame, credits);
+				} catch (InterruptedException e1) {
+					e1.printStackTrace();
+				}
+				
+				CREDITS_scaled.setIcon(new ImageIcon(Scaling.get(CREDITS, 267, 55, Options.full_screen)));
+				revalidate();
+				repaint();
+			}
+
+			@Override
+			public void mouseExited(MouseEvent e) {
+				CREDITS_scaled.setIcon(new ImageIcon(Scaling.get(CREDITS, 267, 55, Options.full_screen)));
+				revalidate();
+				repaint();
+			}
+
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				
+				synchronized(this) {
+					Music.playTone("hover");
+				}
+				
+				CREDITS_scaled.setIcon(new ImageIcon(Scaling.get(CREDITS_SELECTED, 267, 55, Options.full_screen)));
+				revalidate();
+				repaint();
+			}
+
+			@Override public void mouseClicked(MouseEvent e) 	{}
+			@Override public void mouseReleased(MouseEvent e) 	{}
+		});
+		
+		EXIT_scaled.addMouseListener(new MouseListener() {
+
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				
+				synchronized(this) {
+					Music.playTone("hover");
+				}
+				
+				EXIT_scaled.setIcon(new ImageIcon(Scaling.get(EXIT_SELECTED, 141, 50, Options.full_screen)));
+				revalidate();
+				repaint();
+			}
+
+			@Override
+			public void mouseExited(MouseEvent e) {
+				EXIT_scaled.setIcon(new ImageIcon(Scaling.get(EXIT, 141, 50, Options.full_screen)));
+				revalidate();
+				repaint();
+			}
+
+			@Override
+			public void mousePressed(MouseEvent e) {
+				
+				synchronized(this) {
+					Music.playTone("select");
+				}
+				
+				System.exit(0);
+			}
+
+			@Override public void mouseReleased(MouseEvent e) 	{}
+			@Override public void mouseClicked(MouseEvent e) 	{}
+		});
+	}
+	
+	private void menu_choices_init () {
+		menu_choices = new JPanel();
+		menu_choices.setLayout(new BoxLayout(menu_choices, BoxLayout.Y_AXIS));
+
+		menu_choices.add(START_scaled);
+		menu_choices.add(MULTI_scaled);
+		menu_choices.add(OPTIONS_scaled);
+		menu_choices.add(CREDITS_scaled);
+		menu_choices.add(EXIT_scaled);
+		
+		menu_choices.setBackground(new Color(0, 0, 0, 0));
+		menu_choices.setBounds((1280 / 2 - 430 / 2), 250, 550, 550);
+	}
+	
+	private void panel_init(JFrame frame) {
+		options  = new Options(frame, this);
+		credits  = new Credits(frame, this);
+		game 	 = new Game(frame, this);
+		multi	 = new Multiplayer(frame, game, this);
+	}
+	
+	public Menu(JFrame frame) {
+		panel_init(frame);
+		image_init();
+		label_init();
+		MouseListener_init(frame);
+		menu_choices_init();
+		
+		this.setLayout(null);
+		this.add(menu_choices);		
 	}
 
 	@Override
