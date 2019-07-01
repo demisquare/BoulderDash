b5a112e54448aeef90a4121ea8bbe54df9b50a22
DM² <48382390+demisquare@users.noreply.github.com>
2019-07-01 21:51:33 +0200

    Fixed music

--- Boulder Dash/src/audio/Music.java
+++ Boulder Dash/src/audio/Music.java
@@ -5,63 +5,42 @@ import java.io.File;
 import menu.Options;
 
 public class Music {
-		
-	public static final AudioPlayer menuSong = new AudioPlayer(
-			"." + File.separator +
-			"resources" + File.separator +
-			"assets" + File.separator +
-			"music" + File.separator +
-			"menu_song" + ".wav");
-	
-	public static final AudioPlayer gameSong = new AudioPlayer(
-			"." + File.separator +
-			"resources" + File.separator +
-			"assets" + File.separator +
-			"music" + File.separator +
-			"game_song" + ".wav");
-	
-	public static final AudioPlayer creditsSong = new AudioPlayer(
-			"." + File.separator +
-			"resources" + File.separator +
-			"assets" + File.separator +
-			"music" + File.separator +
-			"credits_song" + ".wav");
-	
-	public static void setSong(AudioPlayer song)
-	{
-		Music.backgroundMusic.stop();
-		//Music.backgroundMusic.rewind();
+
+	public static final AudioPlayer menuSong = new AudioPlayer("." + File.separator + "resources" + File.separator
+			+ "assets" + File.separator + "music" + File.separator + "menu_song" + ".wav");
+
+	public static final AudioPlayer gameSong = new AudioPlayer("." + File.separator + "resources" + File.separator
+			+ "assets" + File.separator + "music" + File.separator + "game_song" + ".wav");
+
+	public static final AudioPlayer creditsSong = new AudioPlayer("." + File.separator + "resources" + File.separator
+			+ "assets" + File.separator + "music" + File.separator + "credits_song" + ".wav");
+
+	public static void setSong(AudioPlayer song) {
+		stop();
 		Music.backgroundMusic = song;
+		start();
 	}
-	
-	public static void start()
-	{
-		if (Options.music) {
-			if (!Music.backgroundMusic.isPlaying()) {
+
+	public static void start() {
+		if(Options.music)
+			if (!Music.backgroundMusic.isPlaying())
 				Music.backgroundMusic.loop();
-			}
-		} else {
-			Music.backgroundMusic.stop();
-			//Music.backgroundMusic.rewind();
-		}
 	}
-	
-	public static void playTone(String name)
-	{
-		AudioPlayer tone = new AudioPlayer(
-				"." + File.separator +
-				"resources" + File.separator +
-				"assets" + File.separator +
-				"music" + File.separator +
-				"sfx" + File.separator +
-				name + ".wav");
-		
+
+	public static void stop() {
+		Music.backgroundMusic.stop();
+	}
+
+	public static void playTone(String name) {
+		AudioPlayer tone = new AudioPlayer("." + File.separator + "resources" + File.separator + "assets"
+				+ File.separator + "music" + File.separator + "sfx" + File.separator + name + ".wav");
+
 		if (Options.music) {
 			if (!tone.isPlaying()) {
 				tone.play();
 			}
 		}
 	}
-	
-	public static AudioPlayer backgroundMusic = null;
+
+	public static AudioPlayer backgroundMusic = Music.menuSong;
 }
